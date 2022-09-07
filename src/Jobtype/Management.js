c3e84f72c9ef7cad15fd44799b259edf4632fc31
koyulim <kul2862@naver.com>
2022-09-07 22:10:03 +0900

    test2

--- src/Jobtype/Management.js
+++ src/Jobtype/Management.js
@@ -3,93 +3,81 @@ import axios from "axios";
 import React, { useEffect, useState, Component } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
-class Management extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            url : '',
-            area : '',
-            jobname : '',
-          sample1List : [],
-        }
-      };
-    
-      areaUpdate(e) {
-        this.setState({ area: e.target.value })
-      }
-      jobnameUpdate(e) {
-        this.setState({ jobname: e.target.value })
-      }
-    
-      getKeywordmanageData = async () => {
-        const res = await axios('/get/keywordmanageData', {
-          method: 'POST',
-          data: {
-            'area': this.state.area,
-            'jobname': this.state.jobname
-          },
-          headers: new Headers()
-        });
-        this.setState({
-          sample1List: res.data
-        })
-      }
+function Management() {
 
-      componentDidMount() {
-        this.getData();
-      }
-    
-      getData = async () => {
-        const res = await axios.get('/get/managedata');
-        this.setState({ 
-          sample1List : res.data
-        })
-      }
+  const [area, setArea] = useState('');
+  const [jobname, setJobname] = useState('');
+  const [sample1List, setSample1List] = useState([]);
 
-render() {
-    const { sample1List } = this.state;
-    return (
-        <>
-        <div className='App'>
+  const areaUpdate = (e) => {
+    //setState({ area: e.target.value })
+    setArea(e.target.value);
+  }
+
+  const jobnameUpdate = (e) => {
+    //setState({ jobname: e.target.value })
+    setJobname(e.target.value);
+  }
+
+
+  const getKeywordmanageData = async () => {
+    const res = await axios('/get/keywordmanageData', {
+      method: 'POST',
+      data: {
+        'area': area,
+        'jobname': jobname
+      }
+    });
+    setSample1List(res.data);
+  }
+
+  const getData = async () => {
+    const res = await axios.get('/get/managedata');
+    setSample1List(res.data);
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+
+  return (
+
+    <>
+      <div className='App'>
         <h2>Management List</h2>
-        <input type='text' maxLength='10' placeholder='지역 검색' onChange={(e) => this.areaUpdate(e)} />
-        <input type='text' maxLength='20' placeholder='가게 이름 검색' onChange={(e) => this.jobnameUpdate(e)}/>
-        <button onClick={this.getKeywordmanageData}>Search</button>
-        <button onClick={this.getData}>ListAll</button>
+        <input type='text' maxLength='10' placeholder='지역 검색' onChange={(e) => areaUpdate(e)} />
+        <input type='text' maxLength='20' placeholder='가게 이름 검색' onChange={(e) => jobnameUpdate(e)} />
+        <button onClick={getKeywordmanageData}>Search</button>
+        <button onClick={getData}>ListAll</button>
 
-        <br/>
-        <br/>
+        <br />
+        <br />
         <h3>지역 | 가게 이름 | 날짜</h3>
-        <hr/>
+        <hr />
 
-
-        {sample1List.length !== 0 ? 
-        sample1List.map( (el, key) => {
-          return(
-              <>
-            <div key={key}>
-              {/* <span>  {el.url} </span>/ */}
-              <span> {el.area} </span> |
-              <span> {el.jobname} </span> |
-              <span> {el.date} </span>
-            </div>
-            <div style={{ padding: '10px' }}>
-                  <button onClick={() => window.open('https://www.albamon.com/recruit/view/gi?AL_GI_No='+ el.url +'&mj_stat=0&optgf=', '_blank')} >자세히 보기</button>
+        {sample1List.length !== 0 ?
+          sample1List.map((el, key) => {
+            return (
+              <div key={key}>
+                <div >
+                  {/* <span>  {el.url} </span>/ */}
+                  <span> {el.area} </span> |
+                  <span> {el.jobname} </span> |
+                  <span> {el.date} </span>
+                </div>
+                <div style={{ padding: '10px' }}>
+                  <button onClick={() => window.open('https://www.albamon.com/recruit/view/gi?AL_GI_No=' + el.url + '&mj_stat=0&optgf=', '_blank')} >자세히 보기</button>
                   <button><a className='href' href='Board'>후기글 보기</a></button>
-            </div>
-            <hr/>
-            </>
-          )
-        })
-        : <div>데이터가 없습니다.</div>}
+                </div>
+                <hr />
+              </div>
+            )
+          })
+          : <div>데이터가 없습니다.</div>}
+      </div>
+    </>
+  )
+}
 
-      </div> 
-        </>
-        
-        
-    );
-}
-  
-  
-}
 export default Management;
--- src/Jobtype/Service.js
+++ src/Jobtype/Service.js
@@ -2,90 +2,84 @@ import axios from "axios";
 import React, { useEffect, useState, Component } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
-class Service extends Component {
-  constructor(props) {
-      super(props);
-      this.state = {
-          url : '',
-          area : '',
-          jobname : '',
-        sample1List : [],
+function Service() {
+
+  const [area, setArea] = useState('');
+  const [jobname, setJobname] = useState('');
+  const [sample1List, setSample1List] = useState([]);
+
+  const areaUpdate = (e) => {
+    //setState({ area: e.target.value })
+    setArea(e.target.value);
+  }
+
+  const jobnameUpdate = (e) => {
+    //setState({ jobname: e.target.value })
+    setJobname(e.target.value);
+  }
+
+  const getKeywordserviceData = async () => {
+    const res = await axios('/get/keywordserviceData', {
+      method: 'POST',
+      data: {
+        'area': area,
+        'jobname': jobname
       }
-    };
-  
-    areaUpdate(e) {
-      this.setState({ area: e.target.value })
-    }
-    jobnameUpdate(e) {
-      this.setState({ jobname: e.target.value })
-    }
-  
-    getKeywordserviceData = async () => {
-      const res = await axios('/get/keywordserviceData', {
-        method: 'POST',
-        data: {
-          'area': this.state.area,
-          'jobname': this.state.jobname
-        },
-        headers: new Headers()
-      });
-      this.setState({
-        sample1List: res.data
-      })
-    }
+    });
+    setSample1List(res.data);
+  }
 
-    componentDidMount() {
-      this.getData();
-    }
-  
-    getData = async () => {
-      const res = await axios.get('/get/servicedata');
-      this.setState({ 
-        sample1List : res.data
-      })
-    }
+  useEffect(() => {
+    getData();
+  }, []);
 
-render() {
-  const { sample1List } = this.state;
+  const getData = async () => {
+    const res = await axios.get('/get/servicedata');
+    // setState({
+    //   sample1List: res.data
+    // })
+    setSample1List(res.data);
+  }
+
   return (
-      <>
-     <div className='App'>
+    <>
+      <div className='App'>
         <h2>Service List</h2>
-        <input type='text' maxLength='10' placeholder='지역 검색' onChange={(e) => this.areaUpdate(e)} />
-        <input type='text' maxLength='20' placeholder='가게 이름 검색' onChange={(e) => this.jobnameUpdate(e)}/>
-        <button onClick={this.getKeywordserviceData}>Search</button>
-        <button onClick={this.getData}>ListAll</button>
+        <input type='text' maxLength='10' placeholder='지역 검색' onChange={(e) => areaUpdate(e)} />
+        <input type='text' maxLength='20' placeholder='가게 이름 검색' onChange={(e) => jobnameUpdate(e)} />
+        <button onClick={getKeywordserviceData}>Search</button>
+        <button onClick={getData}>ListAll</button>
 
-        <br/>
-        <br/>
+        <br />
+        <br />
         <h3>지역 | 가게 이름 | 날짜</h3>
-        <hr/>
-        
-      {sample1List.length !== 0 ? 
-      sample1List.map( (el, key) => {
-        return(
-            <>
-          <div key={key}>
-            {/* <span>  {el.url} </span>/ */}
-            <span> {el.area} </span> |
-              <span> {el.jobname} </span> |
-              <span> {el.date} </span>
-          </div>
-          <div style={{ padding: '10px' }}>
-                <button onClick={() => window.open('https://www.albamon.com/recruit/view/gi?AL_GI_No='+ el.url +'&mj_stat=0&optgf=', '_blank')} >자세히 보기</button>
-                <button><a className='href' href='Board'>후기글 보기</a></button>
-          </div>
-          <hr/>
-          </>
-        )
-      })
-      : <div>데이터가 없습니다.</div>}
+        <hr />
 
-    </div> 
-      </>
-      
-      
-  );
+        {sample1List.length !== 0 ?
+          sample1List.map((el, key) => {
+            return (
+              <div key={key}>
+                <div >
+                  {/* <span>  {el.url} </span>/ */}
+                  <span> {el.area} </span> |
+                  <span> {el.jobname} </span> |
+                  <span> {el.date} </span>
+                </div>
+                <div style={{ padding: '10px' }}>
+                  <button onClick={() => window.open('https://www.albamon.com/recruit/view/gi?AL_GI_No=' + el.url + '&mj_stat=0&optgf=', '_blank')} >자세히 보기</button>
+                  <button><a className='href' href='Board'>후기글 보기</a></button>
+                </div>
+                <hr />
+              </div>
+            )
+          })
+          : <div>데이터가 없습니다.</div>}
+
+      </div>
+    </>
+
+  )
+
 }
-}
+
 export default Service;
